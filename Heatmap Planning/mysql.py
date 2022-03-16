fad8a460350f76432dd95874425bb08c043ee5c0
tomitola01 <88623189+tomitola01@users.noreply.github.com>
2022-03-16 20:35:12 +0000

    Update mysql.py

--- Heatmap Planning/mysql.py
+++ Heatmap Planning/mysql.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
-myDB = MySQLdb.connect(host="localhost", port = 3306, user = "tolu", passwd="12345", db = "servel")
+myDB = MySQLdb.connect(host="localhost", port = 3306, user = "tolu", passwd="xxxxx", db = "servel")
 cHandler = myDB.cursor()
 cHandler.execute("SELECT id_circunscripcion_electoral, count(*) as NUM FROM padron_electoral GROUP BY id_circunscripcion_electoral;")
 results = cHandler.fetchall()
